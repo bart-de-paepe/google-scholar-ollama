f64bd832f734bf81bb73bfa6bc4ade60ef3fcce4
bart.de.paepe <bart.de.paepe@vliz.be>
2025-04-03 13:56:02 +0200

    line separator

--- custom-entrypoint.sh
+++ custom-entrypoint.sh
@@ -1,16 +1,1 @@
-#!/bin/bash
-
-# Start Ollama in the background.
-ollama serve &
-# Record Process ID.
-pid=$!
-
-# Pause to allow Ollama to start.
-sleep 5
-
-echo "🔴 Retrieve gemma3:1b model..."
-ollama pull gemma3:27b
-echo "🟢 Done!"
-
-# Wait for the Ollama process to finish.
-wait $pid
+#!/bin/bash# Start Ollama in the background.ollama serve &# Record Process ID.pid=$!# Pause to allow Ollama to start.sleep 5echo "🔴 Retrieve gemma3:1b model..."ollama pull gemma3:1becho "🟢 Done!"# Wait for the Ollama process to finish.wait $pid
